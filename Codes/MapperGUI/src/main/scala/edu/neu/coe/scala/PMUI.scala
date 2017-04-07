174cd5e546932567a94eb86d78b66371ddf66fb6
Tianqi Zheng <zhengtianqi94@163.com>
2017-04-07 12:26:12 -0400

    RDD for ML
    Change of some parts of the GUI, and prepare for the ML part of code.
    Use the RDD and we have to implement TODOs.
    The required data structure has been specified in comments.

--- Codes/MapperGUI/src/main/scala/edu/neu/coe/scala/PMUI.scala
+++ Codes/MapperGUI/src/main/scala/edu/neu/coe/scala/PMUI.scala
@@ -4,122 +4,109 @@ package edu.neu.coe.scala
 import java.awt.Dimension
 import java.io.File
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{Row, SQLContext}
 
 import scala.collection.mutable.ListBuffer
 import scala.swing._
 import scala.swing.event.ButtonClicked
 
-object PMUI extends SimpleSwingApplication{
+object PMUI extends SimpleSwingApplication {
 
   // File direction from the user input filed, use as a variable
   // Path variable
   var dir = new ListBuffer[String]()
 
-  val button = new Button{
+  val button = new Button {
     text = "Submit"
   }
 
-  val label1 = new Label{
-    text = "Ploty Username: "
+  val label1 = new Label {
+    text = "Ploty Username"
   }
 
-  val textf1 = new TextField(){
-    maximumSize = new Dimension(200,30)
+  val textf1 = new TextField() {
+    maximumSize = new Dimension(200, 30)
   }
 
-  val label2 = new Label{
-    text = "Ploty API Key: "
+  val label2 = new Label {
+    text = "Ploty API Key"
   }
 
-  val textf2 = new TextField(){
-    maximumSize = new Dimension(200,30)
+  val textf2 = new TextField() {
+    maximumSize = new Dimension(200, 30)
   }
 
-  val label3 = new Label{
-    text = "File Location: "
+  val textf3 = new TextField() {
+    maximumSize = new Dimension(200, 30)
   }
 
-  val textf3 = new TextField(){
-    maximumSize = new Dimension(200,30)
-  }
-
-  val panel1 = new GridPanel(1,2){
+  val panel1 = new GridPanel(1, 2) {
     contents += label1
     contents += textf1
-    maximumSize = new Dimension(300,30)
+    maximumSize = new Dimension(300, 30)
   }
 
-  val panel2 = new GridPanel(1,2){
+  val panel2 = new GridPanel(1, 2) {
     contents += label2
     contents += textf2
-    maximumSize = new Dimension(300,30)
-  }
-
-  val panel3 = new GridPanel(1,2){
-    contents += label3
-    contents += textf3
-    maximumSize = new Dimension(300,30)
-  }
-
-  val panel4 = new BoxPanel(Orientation.Vertical){
-    contents += panel3
+    maximumSize = new Dimension(300, 30)
   }
 
   var fileChooser = new FileChooser(new File("."))
 
   fileChooser.title = "fileChooser"
 
-  val Choose = new Button {
-    text = "Choose a File from local 1";
+  val Choose1 = new Button {
+    text = "Please choose source data file";
   }
 
   val Choose2 = new Button {
-    text = "Choose a File from local 2"
+    text = "Please choose source data file"
   }
 
   val Choose3 = new Button {
-    text = "Choose a File from local 3"
+    text = "Please choose source data file"
   }
 
   val Choose4 = new Button {
-    text = "Choose a File from local 4"
+    text = "Please choose source data file"
   }
 
   val Choose5 = new Button {
-    text = "Choose a File from local 5"
+    text = "Please choose source data file"
   }
 
 
-  val panel5 = new FlowPanel{
-    contents += Choose
-    maximumSize = new Dimension(300,30)
+  val panel5 = new FlowPanel {
+    contents += Choose1
+    maximumSize = new Dimension(300, 30)
   }
-  val panel6 = new FlowPanel{
+  val panel6 = new FlowPanel {
     contents += Choose2
-    maximumSize = new Dimension(300,30)
+    maximumSize = new Dimension(300, 30)
   }
-  val panel7 = new FlowPanel{
+  val panel7 = new FlowPanel {
     contents += Choose3
-    maximumSize = new Dimension(300,30)
+    maximumSize = new Dimension(300, 30)
   }
-  val panel8 = new FlowPanel{
+  val panel8 = new FlowPanel {
     contents += Choose4
-    maximumSize = new Dimension(300,30)
+    maximumSize = new Dimension(300, 30)
   }
-  val panel9 = new FlowPanel{
+  val panel9 = new FlowPanel {
     contents += Choose5
-    maximumSize = new Dimension(300,30)
+    maximumSize = new Dimension(300, 30)
   }
 
   // This is the main function, which replaces the main(Args[String]) function with MainFrame
-  def top = new MainFrame{
+  def top = new MainFrame {
 
     title = "PM2.5 Mapper"
 
     // Configuration of a new Spark config file and set the starting memory to be used
-    val conf = new SparkConf().setAppName("PM2.5 Mapper").setMaster("local[2]").set("spark.executor.memory","512m");
+    val conf = new SparkConf().setAppName("PM2.5 Mapper").setMaster("local[2]").set("spark.executor.memory", "512m");
 
     // Declare a new SparkContext
     val sc = new SparkContext(conf)
@@ -127,33 +114,32 @@ object PMUI extends SimpleSwingApplication{
     // Declare a new sqlContext
     val sqlContext = new SQLContext(sc)
 
-    contents = new BoxPanel(Orientation.Vertical){
+    contents = new BoxPanel(Orientation.Vertical) {
       contents += panel1
       contents += panel2
-      contents += panel4
       contents += panel5
       contents += panel6
       contents += panel7
       contents += panel8
       contents += panel9
       contents += button
-      border = Swing.EmptyBorder(20,20,20,20)
+      border = Swing.EmptyBorder(20, 20, 20, 20)
     }
 
-    size = new Dimension(300,300)
+    size = new Dimension(400, 400)
 
     listenTo(button)
-    listenTo(Choose)
+    listenTo(Choose1)
     listenTo(Choose2)
     listenTo(Choose3)
     listenTo(Choose4)
     listenTo(Choose5)
 
     reactions += {
-      case ButtonClicked(Choose) => {
+      case ButtonClicked(Choose1) => {
         val result = fileChooser.showOpenDialog(panel5);
         if (result == FileChooser.Result.Approve) {
-          Choose.text = fileChooser.selectedFile.getPath;
+          Choose1.text = fileChooser.selectedFile.getPath;
           dir.+=(fileChooser.selectedFile.getPath);
         }
       }
@@ -186,14 +172,6 @@ object PMUI extends SimpleSwingApplication{
         }
       }
       case ButtonClicked(button) => {
-        println("Ploty Username is " + textf1.text)
-        println("Ploty API Key is " + textf2.text)
-        println("File Location is " + textf3.text)
-        println("File Location is " + Choose.text)
-        println("File Location is " + Choose2.text)
-        println("File Location is " + Choose3.text)
-        println("File Location is " + Choose4.text)
-        println("File Location is " + Choose5.text)
 
         // Declare the operation of the sqlContext which here is read
         val df = sqlContext.read
@@ -202,10 +180,25 @@ object PMUI extends SimpleSwingApplication{
           .option("inferSchema", "true") // Automatically infer data types
           .load(dir.toList.mkString(","))
 
-        // Test if the file is correctly read in
-        val selectedData = df.select("State Name", "City Name")
-        selectedData.write.format("com.databricks.spark.csv").save("../new.csv")
-        selectedData.show()
+        //        Latitude
+        //        Longitude
+        //        Date Local
+        //        Arithmetic Mean - RDD label
+        //        State Name
+        //        City Name
+
+        // Select required fields from source datafile
+        val selectedData = df.select("Latitude", "Longitude", "Date Local", "Arithmetic Mean", "State Name", "City Name")
+
+        // Generate the dataframe to RDD structure for further use
+        val rows: RDD[Row] = selectedData.rdd
+
+        //TODO here we need to form a RDD with label Arithmetic Mean, and characters: Latitude, Longitude, Date Local
+        //TODO this one should be implemented, and after that this RDD can be directly used as training data for macihne learning
+
+        // Test for rdd contents
+        //        rows.take(2).foreach(println)
+
       }
     }
   }
